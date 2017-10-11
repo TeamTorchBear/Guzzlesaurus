7cdb6a919ab5a41f2c9e34a7860e960a8971b977
Pablo Tomico Pérez <pablotomico@hotmail.com>
2017-10-11 11:52:19 +0100

    Implementing some scripts for testing

---
+++ Assets/Scripts/CutControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutControl : Interactable {
+
+    private LineRenderer lineRenderer;
+    private List<Vector2> linePoints = new List<Vector2>();
+
+
+
+    public override bool OnInteractionStart(Vector3 position) {
+        return true;
+    }
+
+    public override void OnInteractionHold(Vector3 position) {
+
+    }
+
+    public override void OnInteractionEnd(Vector3 position) {
+
+    }
+
+    private void Awake() {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void FixedUpdate() {
+
+    }
+
+}
---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour {
+    public abstract bool OnInteractionStart(Vector3 position);
+    public abstract void OnInteractionHold(Vector3 position);
+    public abstract void OnInteractionEnd(Vector3 position);
+}
