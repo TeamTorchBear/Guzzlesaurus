a4584bd6dd7c271e6fcd1235402a9c4a30a2514b
xyskin <xyskin@163.com>
2017-10-23 11:31:28 +0100

    11.31

---
+++ Assets/Scripts/IfAnswerIsCorrect.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Object/Answer ")]
+public class IfAnswerIsCorrect : ScriptableObject {
+    public string minigame;
+    public string answer;
+	
+}
---
+++ Assets/Scripts/LoadScriptableObject.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public static class LoadScriptableObject {
+    // Use this for initialization
+    public static IfAnswerIsCorrect Load (string minigameName) {
+        IfAnswerIsCorrect answer = Resources.Load<IfAnswerIsCorrect>(minigameName);
+        return answer;
+    }
+}
