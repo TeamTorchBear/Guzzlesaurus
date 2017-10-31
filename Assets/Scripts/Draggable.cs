9770f49e2e3725585caa99f1f4745f6a5af20746
unknown <pablotomico@hotmail.com>
2017-10-31 10:47:33 +0000

    Implemented first scripts for minigame 2

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Draggable : Interactable {
 
+    public bool returnToPosition = false;
+    public float returnSpeed = 20f;
     private BoxCollider2D boxCollider;
     private Vector3 offset;
     private bool dragging = false;
+
+    [HideInInspector]
+    public Vector3 initialPosition;
 
     private void Awake() {
         boxCollider = GetComponent<BoxCollider2D>();
@@ -16,6 +21,7 @@ public class Draggable : Interactable {
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(position);
         Vector2 touchPos = new Vector2(worldPos.x, worldPos.y);
         if (boxCollider == Physics2D.OverlapPoint(touchPos)) {
+            initialPosition = transform.position;
             dragging = true;
             offset = boxCollider.bounds.center - worldPos;
             OnDragStart();
@@ -32,6 +38,9 @@ public class Draggable : Interactable {
 
     public override void OnInteractionEnd(Vector3 position) {
         if (dragging) {
+            if (returnToPosition) {
+                MoveTo(initialPosition, false);
+            }
             dragging = false;
             OnDragEnd();
         }
@@ -46,5 +55,29 @@ public class Draggable : Interactable {
 
     private void OnDestroy() {
         dragging = false;
+    }
+
+    public void MoveTo(Vector2 position, bool destroyAfter) {
+        StartCoroutine(AnimatePosition(position, destroyAfter));
+    }
+
+
+    private IEnumerator AnimatePosition(Vector3 finalPos, bool destroyAfter) {
+        float startTime = Time.time;
+        Vector3 initialPos = transform.position;
+        float distance = Vector3.Distance(initialPos, finalPos);
+        float distCovered = 0, fracJourney = 0;
+        if (distance > 0) {
+            while (fracJourney < 1) {
+                distCovered = (Time.time - startTime) * returnSpeed;
+                fracJourney = distCovered / distance;
+                transform.position = Vector3.Lerp(initialPos, finalPos, fracJourney);
+                yield return false;
+            }
+            transform.position = finalPos;
+            if (destroyAfter) {
+                Destroy(this.gameObject);
+            }
+        }
     }
 }
