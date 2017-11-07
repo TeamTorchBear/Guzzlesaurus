85c92ea2d6a32abf79880ae050dfc5c0a38af7cc
xyskin <xyskin@163.com>
2017-11-07 10:58:56 +0000

    fixbugs

--- Assets/Scripts/Initialize.cs
+++ Assets/Scripts/Initialize.cs
@@ -8,13 +8,13 @@ public class Initialize:MonoBehaviour {
 
     private void Start()
     {
-        if (!File.Exists(Application.dataPath + "/Data/Inventory.dat"))
+        if (!File.Exists(Application.dataPath + "/Inventory.txt"))
         {
             BinaryFormatter bf = new BinaryFormatter();
             Data data = new Data();
             data.moneyWeHave = 500;
-            File.Create(Application.dataPath + "/Data/Inventory.dat").Dispose();
-            FileStream file = File.Open(Application.dataPath + "/Data/Inventory.dat", FileMode.Open);
+            File.Create(Application.dataPath + "/Inventory.txt").Dispose();
+            FileStream file = File.Open(Application.dataPath + "/Inventory.txt", FileMode.Open);
             bf.Serialize(file, data);
             file.Close();
         }
