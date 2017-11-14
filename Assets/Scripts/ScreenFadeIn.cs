480ff5ab6f7877a9997215dcf7420d26cebd38d4
xyskin <xyskin@163.com>
2017-11-14 09:05:11 +0000

    dunno

---
+++ Assets/Scripts/ScreenFadeIn.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenFadeIn : MonoBehaviour {
+
+    public Image screen;
+	// Use this for initialization
+	void Start () {
+        screen.gameObject.SetActive(true);
+        screen.color = new Color(screen.color.r,screen.color.g,screen.color.b,1.0f);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (screen.gameObject.activeSelf)
+        {
+            if (screen.color.a >= 0)
+                screen.color = new Color(screen.color.r, screen.color.g, screen.color.b, screen.color.a - 0.02f);
+            else
+            {
+                screen.gameObject.SetActive(false);
+                this.GetComponent<ScreenFadeIn>().enabled = false;
+            }
+        }
+    }
+}
