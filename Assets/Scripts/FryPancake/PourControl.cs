21bf538be0c5c148cfda9b987170a9c05140eb4c
xyskin <xyskin@163.com>
2017-11-22 14:18:09 +0000

    minigame4 scripts

---
+++ Assets/Scripts/FryPancake/PourControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PourControl : MonoBehaviour {
+
+    public float pourTime;
+	// Use this for initialization
+	void Start () {
+        pourTime = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!this.GetComponent<BowlDownFromRight>().enabled)
+        {
+            if (Input.acceleration.x > 30)
+            {
+                StartPour();
+                pourTime += Time.deltaTime;
+            }
+            else
+            {
+                StopPour();
+            }
+
+            if (Input.GetKey(KeyCode.Space))
+            {
+                StartPour();
+            }
+            else
+            {
+                StopPour();
+            }
+        }
+    }
+
+    void StartPour()
+    {
+        Debug.Log("Start Pouring");
+    }
+
+    void StopPour()
+    {
+        Debug.Log("Stop Pouring");
+    }
+}
