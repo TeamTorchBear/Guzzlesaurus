ff117b0465f5bafe680e2ffa0a9cdb9e784e5b0a
xyskin <xyskin@163.com>
2017-11-23 13:00:04 +0000

    dunno why missing push

---
+++ Assets/Scripts/FryPancake/BowlLeave.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlLeave : MonoBehaviour
+{
+    public Transform startMarker;
+    public Transform endMarker;
+    public float speed = 10F;
+    private float startTime;
+    private float journeyLength;
+    // Use this for initialization
+    void Start()
+    {
+        //this.transform.position = startMarker.position;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+        this.transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fracJourney);
+        if (this.transform.position.y == endMarker.position.y)
+        {
+            this.GetComponent<BowlLeave>().enabled = false;
+            //FindObjectOfType<VibrateControl>().enabled = true;
+            FindObjectOfType<ReplacePancake>().enabled = true;
+        }
+
+    }
+}
