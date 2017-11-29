50e0697dcdb360cafba26fc2d49f572f220bd95c
Pablo Tomico <pablotomico@hotmail.com>
2017-11-29 11:19:59 +0000

    CaveScreen hotfix

--- Assets/Scripts/CookBookBtn.cs
+++ Assets/Scripts/CookBookBtn.cs
@@ -13,68 +13,53 @@ public class CookBookBtn : Clickable {
 
     bool isClick;
     // Use this for initialization
-    public override void OnStart()
-    {
+    public override void OnStart() {
         isClick = false;
         Button btn = this.GetComponent<Button>();
+        if (btn != null) {
 
-        btn.onClick.AddListener(OnClick);
+            btn.onClick.AddListener(OnClick);
+        }
 
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (isClick)
-        {
-            if (!jbc.isFireflies)
-            {
+    void Update() {
+        if (isClick) {
+            if (!jbc.isFireflies) {
                 ScreenFadeOut("CookBookScene");
-            }
-            else
-            {
+            } else {
                 isClick = false;
             }
         }
 
     }
 
-    public override void OnClick()
-    {
+    public override void OnClick() {
         Debug.Log("book clicked");
         isClick = true;
         AkSoundEngine.PostEvent("Click_Recipe", gameObject);
         GetComponent<Animator>().Play("cs_bookTapNew");
     }
 
-    void ScreenFadeOut(string scene)
-    {
+    void ScreenFadeOut(string scene) {
         FindObjectOfType<ScreenFadeIn>().enabled = false;
-        if (Screen != null)
-        {
+        if (Screen != null) {
 
             Screen.gameObject.SetActive(true);
-            if (Screen.color.a <= 1.0f)
-            {
+            if (Screen.color.a <= 1.0f) {
                 Screen.color = new Color(Screen.color.r, Screen.color.g, Screen.color.b, Screen.color.a + 0.02f);
-            }
-            else
-            {
+            } else {
                 isClick = false;
                 //Screen.gameObject.SetActive(false);
 
                 SceneManager.LoadScene(scene);
             }
-        }
-        else if (spriteRenderer != null)
-        {
+        } else if (spriteRenderer != null) {
             spriteRenderer.gameObject.SetActive(true);
-            if (spriteRenderer.color.a <= 1.0f)
-            {
+            if (spriteRenderer.color.a <= 1.0f) {
                 spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, spriteRenderer.color.a + 0.02f);
-            }
-            else
-            {
+            } else {
                 isClick = false;
                 //Screen.gameObject.SetActive(false);
                 Debug.Log(scene);
