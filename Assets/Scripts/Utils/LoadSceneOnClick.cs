1faf5f3cb63626d0c5474c8746bb7725231e7730
Pablo Tomico Pérez <pablotomico@hotmail.com>
2018-02-06 14:42:33 +0000

    Implemented SceneLoader and LoadSceneOnClick for easier implementation of UI links

---
+++ Assets/Scripts/Utils/LoadSceneOnClick.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LoadSceneOnClick : Clickable {
+
+    [SerializeField]
+    private string sceneName;
+
+    private SceneLoader sceneLoader;
+
+    public override void OnStart() {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    public override void OnClick() {
+        sceneLoader.LoadScene(sceneName);
+    }
+
+
+}
---
+++ Assets/Scripts/Utils/SceneLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using System;
+
+public class SceneLoader : MonoBehaviour {
+    
+    [SerializeField]
+    private bool transitions = true;
+
+	[SerializeField]
+	private float transitionTime = 1f;
+
+    [SerializeField]
+    private SpriteRenderer image;
+
+    public void LoadScene(string sceneName) {
+        StartCoroutine(TransitionCoroutine(() => { SceneManager.LoadScene(sceneName); }));
+    }
+
+
+
+    private IEnumerator TransitionCoroutine(Action function = null) {
+        float startTime = Time.time;
+        image.enabled = true;
+        image.color = new Color(0f, 0f, 0f, 0f);
+
+        while (Time.time - startTime < transitionTime) {
+            image.color = new Color(0f, 0f, 0f, Time.time - startTime / transitionTime);
+            yield return false;
+        }
+        image.color = new Color(0f, 0f, 0f, 1f);
+
+        if (function != null) {
+            function();
+        }
+    }
+}
