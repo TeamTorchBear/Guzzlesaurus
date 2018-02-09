cfca47c064adc6cfa4bf8f68fb6ab735d0b66695
Pablo Tomico <pablotomico@hotmail.com>
2018-02-09 15:33:05 +0000

    Implemented Scale animation coroutine

--- Assets/Scripts/Utils/Coroutines.cs
+++ Assets/Scripts/Utils/Coroutines.cs
@@ -23,4 +23,23 @@ public static class Coroutines {
             function();
         }
     }
+
+    public static IEnumerator AnimateScale(GameObject objToScale, Vector3 finalScale, float speed, Action function = null) {
+        float startTime = Time.time;
+        Vector3 initialScale = objToScale.transform.localScale;
+        float distance = Vector3.Distance(initialScale, finalScale);
+        float distCovered = 0, fracJourney = 0;
+        if (distance > 0) {
+            while (fracJourney < 1) {
+                distCovered = (Time.time - startTime) * speed;
+                fracJourney = distCovered / distance;
+                objToScale.transform.localScale = Vector3.Lerp(initialScale, finalScale, fracJourney);
+                yield return false;
+            }
+            objToScale.transform.localScale = finalScale;
+        }
+        if (function != null) {
+            function();
+        }
+    }
 }
