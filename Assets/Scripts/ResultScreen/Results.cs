807adb22448174cd24191cee27e71a733d95b8cc
Pablo Tomico <pablotomico@hotmail.com>
2018-02-09 15:37:03 +0000

    Updated results screen controls

--- Assets/Scripts/ResultScreen/Results.cs
+++ Assets/Scripts/ResultScreen/Results.cs
@@ -8,12 +8,14 @@ public class Results : MonoBehaviour {
     [SerializeField]
     private GameObject[] stars;
     [SerializeField]
-    private SwipeScreen swipeControl;
+    private TapScreenControl tapScreenControl;
     [SerializeField]
     private GameObject coin;
 
     private int starIndex = 0;
     private int score;
+
+    private bool tapOnce;
 
     private void Start() {
         Data data = SaveNLoadTxt.Load();
@@ -25,14 +27,32 @@ public class Results : MonoBehaviour {
             stars[starIndex].GetComponent<Animator>().Play("PopStar");
             starIndex++;
         } else {
-            swipeControl.GetComponent<BoxCollider2D>().enabled = true;
+            tapScreenControl.GetComponent<BoxCollider2D>().enabled = true;
         }
     }
 
     public void ShowCoin() {
-        //coin.SetActive(true);
+        coin.SetActive(true);
         foreach (Animator anim in coin.GetComponentsInChildren<Animator>()) {
             anim.Play("Play");
+        }
+    }
+
+    public void ScreenTap() {
+        if (!tapOnce) {
+            GetComponent<Animator>().enabled = false;
+            StartCoroutine(Coroutines.AnimateScale(gameObject, Vector3.zero, 10, () => {
+                ShowCoin();
+            }));
+            foreach (GameObject star in stars) {
+                StartCoroutine(Coroutines.AnimateScale(star, Vector3.zero, 10));
+            }
+            
+
+            tapOnce = true;
+
+        } else {
+            FindObjectOfType<SceneLoader>().LoadScene("StartScreen");
         }
     }
 
---
+++ Assets/Scripts/ResultScreen/TapScreenControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapScreenControl : Clickable {
+
+    private Results results;
+
+    
+    public override void OnStart() {
+        results = FindObjectOfType<Results>();
+    }
+
+    public override void OnClick() {
+        results.ScreenTap();
+    }
+    
+}
